1d48b51e6e6ca8d1594c5f5a4253cb9076569144
gchamb <gianttechambers@gmail.com>
2024-08-26 17:04:59 -0500

    feat: :sparkles: complete onboarding integration with backend

--- app/(auth)/onboarding.tsx
+++ app/(auth)/onboarding.tsx
@@ -4,6 +4,9 @@ import Dropdown from "@/components/ui/dropdown";
 import EgoistView from "@/components/ui/egoist-view";
 import sanitizedConfig from "@/config";
 import * as ExpoImagePicker from "expo-image-picker";
+import { authAtom } from "@/stores/auth";
+import { router } from "expo-router";
+import { useAtomValue } from "jotai/react";
 import { useState } from "react";
 import { Text, View } from "react-native";
 
@@ -14,21 +17,39 @@ export default function Onboarding() {
   const [currentWeight, setCurrentWeight] = useState<number>();
   const [progressImage, setProgressImage] =
     useState<ExpoImagePicker.ImagePickerAsset | null>(null);
+  const authTokens = useAtomValue(authAtom);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
   const completeOnboarding = async () => {
-    if (progressImage === null) {
-      // toast the error
+    if (
+      progressImage === null ||
+      currentWeight === undefined ||
+      goalWeight === undefined
+    ) {
+      setError("You must fill out all the details");
       return;
+    }
+    if (error !== "") {
+      setError("");
     }
 
     try {
+      setLoading(true);
+
       // request for sas url
       const sasResponse = await fetch(
-        `${sanitizedConfig.API_URL}/api/v1/azure/upload`
+        `${sanitizedConfig.API_URL}/api/v1/azure/upload?mimetype=${progressImage.mimeType}`,
+        {
+          headers: {
+            Authorization: `Bearer ${authTokens?.jwt_token}`,
+          },
+        }
       );
 
       if (!sasResponse.ok) {
         // throw some error
+        throw new Error("Unable to upload image. Try Again.");
       }
       const sasData = (await sasResponse.json()) as {
         key: string;
@@ -36,27 +57,47 @@ export default function Onboarding() {
       };
 
       // upload image
+      const imageFetch = await fetch(progressImage.uri);
+      const image = await imageFetch.blob();
+
       const sasUploadRes = await fetch(sasData.url, {
         method: "PUT",
+        body: image,
         headers: {
           "x-ms-blob-type": "BlockBlob",
         },
       });
 
       if (!sasUploadRes.ok) {
-        // throw some error
+        throw new Error("Unable to upload image. Try Again.");
       }
 
       // onboard the user
       const onboardedRes = await fetch(
-        `${sanitizedConfig.API_URL}/api/user/onboard`,
+        `${sanitizedConfig.API_URL}/api/v1/user/onboard`,
         {
           method: "PATCH",
-          body: JSON.stringify({ key: sasData.key, currentWeight, goalWeight }),
+          body: JSON.stringify({
+            key: sasData.key,
+            current_weight: currentWeight,
+            goal_weight: goalWeight,
+          }),
+          headers: {
+            Authorization: `Bearer ${authTokens?.jwt_token}`,
+          },
         }
       );
+
+      if (!onboardedRes.ok) {
+        throw new Error("Unable to onboard user. Try Again.");
+      }
       // direct to the home screen
-    } catch (err) {}
+      router.replace("/(tabs)/home");
+    } catch (err) {
+      setError(err instanceof Error ? err.message : String(err));
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
@@ -117,7 +158,16 @@ export default function Onboarding() {
             </View>
           </View>
 
-          <Button className="p-6" text="Start" />
+          <Button
+            className="p-6"
+            text="Start"
+            onPress={completeOnboarding}
+            disabled={loading}
+            isLoading={loading}
+          />
+          {error !== "" && (
+            <Text className="text-sm text-red-700 text-center">{error}</Text>
+          )}
         </View>
       </View>
     </EgoistView>
